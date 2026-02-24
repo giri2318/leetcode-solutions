cd58b24bdefc9ef10849de571052568c421329f9
Giridharan27 <giriarul4@gmail.com>
2026-02-24 17:53:49 +0530

    File updated at 2/24/2026, 12:23:49 PM

--- 242-ValidAnagram/242-ValidAnagram.c
+++ 242-ValidAnagram/242-ValidAnagram.c
@@ -1,19 +1,12 @@
-// Last updated: 2/24/2026, 12:08:44 PM
-1bool isAnagram(char* s, char* t) {
-2    int S = strlen(s), T = strlen(t);
-3    if (S != T) return false;
-4
-5    int c[26] = {0};  
-6
-7    for (int i = 0; i < S; i++) {
-8        c[s[i] - 'a']++;  
-9        c[t[i] - 'a']--;  
-10    }
-11
-12    for (int i = 0; i < 26; i++) {
-13        if (c[i] != 0){
-14            return false;
-15        } 
-16    }
-17    return true;
-18}
+// Last updated: 2/24/2026, 12:23:49 PM
+1int firstUniqChar(char* s) {
+2    int n = strlen(s);
+3    int c[26] = {0};
+4    for(int i = 0; i < n; i++){
+5        c[s[i]-'a']++;
+6    }   
+7    for(int i = 0; i < n; i++){
+8        if((c[s[i]-'a'])==1) return i;
+9    }
+10    return -1;
+11}
