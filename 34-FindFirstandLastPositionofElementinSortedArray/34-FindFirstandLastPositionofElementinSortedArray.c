11ee6c5f019e305db9be8fb615379cafbf5ee6fb
Giridharan27 <giriarul4@gmail.com>
2026-03-25 18:46:47 +0530

    File updated at 3/25/2026, 6:46:47 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.c
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.c
@@ -1,20 +1,8 @@
-// Last updated: 3/25/2026, 6:37:02 PM
-1int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
-2    *returnSize = 2;
-3    int *result = malloc(2 * sizeof(int));
-4
-5    result[0] = -1;
-6    result[1] = -1;
-7
-8    for (int i = 0; i < numsSize; i++) {
-9        if (nums[i] == target) 
-10        {
-11            if (result[0] == -1)
-12            {
-13                result[0] = i;   
-14            }
-15            result[1] = i;      
-16        }
-17    }
-18    return result;
-19}
+// Last updated: 3/25/2026, 6:46:47 PM
+1int searchInsert(int* nums, int numsSize, int target) {
+2    for (int i = 0; i < numsSize; i++) {
+3        if (nums[i] >= target)
+4            return i;
+5    }
+6    return numsSize; 
+7}
